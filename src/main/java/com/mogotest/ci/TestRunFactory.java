b1142720e2304213a8e2780d1921075537ec5e4d
Kevin Menard <nirvdrum@gmail.com>
2013-03-15 22:01:03 -0400

    Small code clean-up.

--- src/main/java/com/mogotest/ci/TestRunFactory.java
+++ src/main/java/com/mogotest/ci/TestRunFactory.java
@@ -30,11 +30,9 @@ public class TestRunFactory
 
   public static void create(final String apiKey, final String testPlanId, final String source)
   {
-    HttpClient client = new HttpClient();
-    // 'https://mogotest.com/api/v1/test_plans/pg329o/tests.json' -d 'user_credentials=CREDENTIALS'
-
-    String url = String.format("https://mogotest.com/api/v1/test_plans/%s/tests.json", testPlanId);
-    PostMethod post = new PostMethod(url);
+    final HttpClient client = new HttpClient();
+    final String url = String.format("https://mogotest.com/api/v1/test_plans/%s/tests.json", testPlanId);
+    final PostMethod post = new PostMethod(url);
 
     try
     {
